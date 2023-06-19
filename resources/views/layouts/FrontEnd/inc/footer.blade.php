a132a2ad2d32532fc1c1afbd720da62de0e7ccc0
HoangVan6655 <89716077+HoangVan6655@users.noreply.github.com>
2023-06-19 23:56:51 +0700

    update

--- resources/views/layouts/FrontEnd/inc/footer.blade.php
+++ resources/views/layouts/FrontEnd/inc/footer.blade.php
@@ -1,15 +1,41 @@
 <footer id="footer" class="clearfix">
     <div class="container footer-columns">
-        <div class="row container">
-            <div class="widget about col-xs-12 col-sm-4 col-md-4">
-                <div class="footer-logo">
-                    <img class="img-responsive"
-                         src="https://img.favpng.com/9/23/19/movie-logo-png-favpng-nRr1DmYq3SNYSLN8571CHQTEG.jpg"
-                         alt="Phim hay 2021- Xem phim hay nhất"/>
+        <div class="row container col-md-12">
+            <div class="col-xs-12 col-sm-4 col-md-3">
+                <div class="widget about">
+                    <div class="footer-logo" style="margin-top: 5px">
+                        <a href="{{ route('homepage') }}">
+                            <img src='{{ asset ('admin/images/logo.png') }}' alt="logo"/>
+                        </a>
+                    </div>
+                    <div class="text-justify">
+                        Trang xem phim Online với giao diện được bố trí và thiết kế thân thiện với người dùng. Nguồn
+                        phim được tổng hợp từ các website lớn với đa dạng các đầu phim và thể loại vô cùng phong phú.
+                    </div>
                 </div>
-                Liên hệ QC: <a href="/cdn-cgi/l/email-protection" class="__cf_email__"
-                               data-cfemail="e5958d8c888d849ccb868aa58288848c89cb868a88">[email&#160;protected]</a>
             </div>
+
+            <div class="col-xs-12 col-sm-4 col-md-3">
+                <div class="ud-widget">
+                    <h4 class="ud-widget-title" style="margin-top: 40px">Danh Mục Phim</h4>
+                    <ul class="ud-widget-links" style="list-style: none; padding-left: 0;">
+                        @foreach($category as $key => $cate)
+                            <li class="mega">
+                                <a title="{{$cate->title}}" href="{{ route('category', $cate->slug) }}">
+                                    {{$cate->title}}
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-12">
+            <p class="ud-footer-bottom-right text-center">
+                Developed by
+                <a href="https://www.facebook.com/HoangVan6501/" rel="nofollow">Hoàng Văn</a>
+            </p>
         </div>
     </div>
 </footer>
--- resources/views/layouts/FrontEnd/inc/header.blade.php
+++ resources/views/layouts/FrontEnd/inc/header.blade.php
@@ -1,43 +1,44 @@
 <header id="header">
     <div class="container">
+
         <div class="row" id="headwrap">
+            <div class="col">
+                <div class="col-md-6 col-sm-6 ">
+                    <a href="{{ route('homepage') }}">
+                        <img src='{{ asset ('admin/images/logo.png') }}' alt="logo"/>
+                    </a>
+                </div>
 
-            <div class="col-md-6 col-sm-6 slogan">
-                <p class="site-title">
-                    <a class="logo" href="" title="phim hay "> Phim Hay </a>
-                </p>
-            </div>
-
-            <div class="col-md-6 col-sm-6 halim-search-form hidden-xs">
-                <div class="header-nav">
-                    <div class="col-xs-12">
-                        <ul class="list-group search-result" id="result" style="display: none;"></ul>
-                        <div class="form-group">
-                            <div class="input-group col-xs-12">
-                                <form id="search-form-pc" name="halimForm" role="search"
-                                      action="{{ route('tim-kiem') }}" method="GET">
-                                    <div class="form-group">
-                                        <div class="input-group col-xs-12">
-                                            <input id="timkiem" type="text" name="search"
-                                                   class="form-control rounded-10"
-                                                   placeholder="Tìm kiếm phim..." autocomplete="off" required>
-                                            <div class="input-group-btn">
-                                                <button class="btn btn-search rounded-10 buttonSearch" type="submit"
-                                                        style="left: 5px;">
-                                                    <i class="fa fa-search"></i> Tìm Kiếm
-                                                </button>
+                <div class="col-md-6 col-sm-6 halim-search-form hidden-xs" style="margin-top: 20px">
+                    <div class="header-nav">
+                        <div class="col-xs-12">
+                            <ul class="list-group search-result" id="result" style="display: none;"></ul>
+                            <div class="form-group">
+                                <div class="input-group col-xs-12">
+                                    <form id="search-form-pc" name="halimForm" role="search"
+                                          action="{{ route('tim-kiem') }}" method="GET">
+                                        <div class="form-group">
+                                            <div class="input-group col-xs-12">
+                                                <input id="timkiem" type="text" name="search"
+                                                       class="form-control rounded-10"
+                                                       placeholder="Tìm kiếm phim..." autocomplete="off" required>
+                                                <div class="input-group-btn">
+                                                    <button class="btn btn-search rounded-10 buttonSearch" type="submit"
+                                                            style="left: 5px;">
+                                                        <i class="fa fa-search"></i> Tìm Kiếm
+                                                    </button>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                </form>
+                                    </form>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-
-
         </div>
+
     </div>
 </header>
 
