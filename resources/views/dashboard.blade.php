908923ff361339903c56d1e11156eb86546e7ca8
HoangVan6655 <89716077+HoangVan6655@users.noreply.github.com>
2023-06-23 01:14:31 +0700

    update UI Admin

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,30 +2,72 @@
 
 @section('content')
     <div class="content-wrapper">
-        <div class="row ">
-            <div class="col-12 grid-margin">
+
+        <div class="row">
+            <div class="col-sm-3 grid-margin">
                 <div class="card">
                     <div class="card-body">
-                        <h4 class="card-title">Thống Kê</h4>
-                        <div class="table-responsive">
-                            <table class="table">
-                                <thead>
-                                <tr>
-                                    <th> Danh Mục Phim</th>
-                                    <th> Thể Loại</th>
-                                    <th> Quốc Gia</th>
-                                    <th> Phim</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <tr>
-                                    <td> Có {{ $categoryCount }} Danh Mục</td>
-                                    <td> Có {{ $genreCount }} Thể Loại</td>
-                                    <td> Có {{ $countryCount }} Quốc Gia</td>
-                                    <td> Có {{ $movieCount }} Phim</td>
-                                </tr>
-                                </tbody>
-                            </table>
+                        <div class="row">
+                            <div class="col-8 col-sm-12 col-xl-8 my-auto">
+                                <div class="d-flex d-sm-block d-md-flex align-items-center">
+                                    <h3 class="mb-0">Danh Mục Phim</h3>
+                                </div>
+                                <h6 class="text-muted font-weight-normal">Có {{ $categoryCount }} Danh Mục Phim</h6>
+                            </div>
+                            <div class="col-4 col-sm-12 col-xl-4 text-center text-xl-right">
+                                <i class="icon-lg mdi mdi-collage text-warning ml-auto"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3 grid-margin">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-8 col-sm-12 col-xl-8 my-auto">
+                                <div class="d-flex d-sm-block d-md-flex align-items-center">
+                                    <h3 class="mb-0">Thể Loại Phim</h3>
+                                </div>
+                                <h6 class="text-muted font-weight-normal"> Có {{ $genreCount }} Thể Loại Phim</h6>
+                            </div>
+                            <div class="col-4 col-sm-12 col-xl-4 text-center text-xl-right">
+                                <i class="icon-lg mdi mdi-shape-plus text-danger ml-auto"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3 grid-margin">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-8 col-sm-12 col-xl-8 my-auto">
+                                <div class="d-flex d-sm-block d-md-flex align-items-center">
+                                    <h3 class="mb-0">Quốc Gia Phim</h3>
+                                </div>
+                                <h6 class="text-muted font-weight-normal">Có {{ $countryCount }} Quốc Gia Phim</h6>
+                            </div>
+                            <div class="col-4 col-sm-12 col-xl-4 text-center text-xl-right">
+                                <i class="icon-lg mdi mdi-airballoon text-primary ml-auto"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3 grid-margin">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-8 col-sm-12 col-xl-8 my-auto">
+                                <div class="d-flex d-sm-block d-md-flex align-items-center">
+                                    <h2 class="mb-0">Phim</h2>
+                                </div>
+                                <h6 class="text-muted font-weight-normal">Có {{ $movieCount }} Phim</h6>
+                            </div>
+                            <div class="col-4 col-sm-12 col-xl-4 text-center text-xl-right">
+                                <i class="icon-lg mdi mdi-monitor text-success ml-auto"></i>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -37,7 +79,9 @@
                 <div class="card">
                     <div class="card-body">
                         <div class="d-flex flex-row justify-content-between">
-                            <h4 class="card-title mb-1">Tổng Phim Theo Từng Danh Mục</h4>
+                            <h4 class="card-title mb-1">Tổng Phim Theo Mỗi Danh Mục</h4>
+                            <a class="nav-link btn btn-success create-new-button" href="{{ route('category.create') }}">+
+                                Thêm Mới</a>
                         </div>
                         <div class="row">
                             <div class="col-12">
@@ -74,6 +118,8 @@
                     <div class="card-body">
                         <div class="d-flex flex-row justify-content-between">
                             <h4 class="card-title mb-1">Tổng Phim Theo Từng Thể Loại</h4>
+                            <a class="nav-link btn btn-success create-new-button" href="{{ route('genre.create') }}">+
+                                Thêm Mới</a>
                         </div>
                         <div class="row">
                             <div class="col-12">
@@ -138,7 +184,19 @@
             <div class="col-12">
                 <div class="card">
                     <div class="card-body">
-                        <h4 class="card-title">Tổng Phim Theo Từng Quốc Gia</h4>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="d-flex align-items-center">
+                                    <h4 class="card-title">Tổng Phim Theo Từng Quốc Gia</h4>
+                                </div>
+                            </div>
+                            <div class="col-md-8">
+                                <div class="d-flex align-items-center justify-content-start">
+                                    <a class="nav-link btn btn-success create-new-button"
+                                       href="{{ route('country.create') }}">+ Thêm Mới</a>
+                                </div>
+                            </div>
+                        </div>
                         <div class="row">
                             <div class="col-md-5">
                                 @foreach($listCountry as $key => $country)
